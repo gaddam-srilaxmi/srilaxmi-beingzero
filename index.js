c411bb5f0d80b5026fe103c3589ae5512d763001
gaddam-srilaxmi <59080467+gaddam-srilaxmi@users.noreply.github.com>
2019-12-20 14:55:24 +0530

    adding project files

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('frontend'))
+app.get('/', (req, res) => res.sendfile(__dirname+'/frontend/html/home.html'))
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
